dde4a89d47c2d9bfd7610ab233a373348c431cf1
pyu-i <nizyoukonn@outlook.jp>
2019-12-30 18:56:22 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/riku/omni/src
+# Build directory: /home/riku/omni/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("omni_pkg")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/riku/omni/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/home/riku/omni/devel/lib:/home/riku/catkin_ws/devel/lib:/opt/ros/melodic/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/lib"
+export PATH="/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/melodic/share/euslisp/jskeus/eus//Linux64/bin:/home/riku/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/riku/omni/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/riku/omni/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/riku/omni/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/riku/omni/src:$ROS_PACKAGE_PATH"
